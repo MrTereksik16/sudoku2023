1739817b03a73a9ac2f738cdae3626475165047b
MrTereksik16 <nikita.rensky@yandex.ru>
2023-05-28 17:07:42 +0300

    sudokus

--- main.cpp
+++ main.cpp
@@ -4,35 +4,35 @@ using namespace std;
 const int SIZE = 9;
 
 void printBoard(int board[SIZE][SIZE]) {
-for (int i = 0; i < SIZE; i++) {
-for (int j = 0; j < SIZE; j++) {
-cout << board[i][j] << " ";
-}
-cout << endl;
-}
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 
 bool isSafe(int board[SIZE][SIZE], int row, int col, int num) {
-for (int i = 0; i < SIZE; i++) {
-if (board[row][i] == num || board[i][col] == num) {
-return false;
-}
-}
-int startRow = row - row % 3;
-int startCol = col - col % 3;
-for (int i = 0; i < 3; i++) {
-for (int j = 0; j < 3; j++) {
-if (board[i + startRow][j + startCol] == num) {
-return false;
-}
-}
-}
+    for (int i = 0; i < SIZE; i++) {
+        if (board[row][i] == num || board[i][col] == num) {
+            return false;
+        }
+    }
+    int startRow = row - row % 3;
+    int startCol = col - col % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i + startRow][j + startCol] == num) {
+                return false;
+            }
+        }
+    }
 
     return true;
 }
 
 bool solveSudoku(int board[SIZE][SIZE]) {
-int row, col;
+    int row, col;
 
     bool isEmpty = false;
     for (row = 0; row < SIZE; row++) {
@@ -67,21 +67,20 @@ int row, col;
 }
 
 int main() {
-int board[SIZE][SIZE] = {
-{5, 3, 0, 0, 7, 0, 9, 0, 2},
-{6, 0, 0, 1, 9, 5, 0, 0, 0},
-{0, 9, 8, 0, 0, 0, 0, 6, 0},
-{8, 0, 0, 0, 6, 0, 4, 0, 3},
-{4, 2, 0, 8, 0, 3, 0, 0, 1},
-{7, 0, 3, 0, 2, 0, 8, 0, 6},
-{0, 6, 0, 0, 0, 0, 2, 8, 0},
-{0, 0, 0, 4, 1, 9, 0, 0, 5},
-{0, 0, 5, 2, 8, 0, 0, 7, 9}
-};
+    int board[SIZE][SIZE] = {
+            {5, 3, 0, 0, 7, 0, 9, 0, 2},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 4, 0, 3},
+            {4, 2, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 3, 0, 2, 0, 8, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 5, 2, 8, 0, 0, 7, 9}
+    };
 
 
-//    solveSudoku(board);
-printBoard(board);
+    printBoard(board);
 
     while (true) {
         int row, col, num;
@@ -89,7 +88,7 @@ printBoard(board);
         cin >> row >> col >> num;
 
         if (row < 1 || row > 9 || col < 1 || col > 9 || num < 1 || num > 9) {
-            cout << "Некорректный ввод.Попробуйте снова." << endl;
+            cout << "Некорректный ввод." << endl;
             continue;
         }
 
@@ -97,7 +96,6 @@ printBoard(board);
             board[row - 1][col - 1] = num;
             printBoard(board);
 
-            // Проверяем, все ли ячейки заполнены пользователем
             bool isComplete = true;
             for (int i = 0; i < SIZE; i++) {
                 for (int j = 0; j < SIZE; j++) {
@@ -116,7 +114,7 @@ printBoard(board);
                 break;
             }
         } else {
-            cout << "Невозможно разместить число в данной позиции.Попробуйте снова." << endl;
+            cout << "Невозможно разместить число в данной позиции." << endl;
         }
     }
     return 0;
